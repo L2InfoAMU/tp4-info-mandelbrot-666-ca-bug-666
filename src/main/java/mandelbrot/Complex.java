8ea79714e21cb308e825cb080f5b4bb4198c9ebe
ashrakss <thenil@hotmail.fr>
2018-11-07 10:39:18 +0100

    Modification de la valeur de I et de la méthode getReal

--- src/main/java/mandelbrot/Complex.java
+++ src/main/java/mandelbrot/Complex.java
@@ -45,10 +45,10 @@ public class Complex {
     /**
      * The complex number whose square is -1
      */
-    static Complex I = new Complex(0, -1);
+    static Complex I = new Complex(0, 1);
 
     double getReal() {
-        return imaginary;
+        return real;
     }
 
     double getImaginary() {
